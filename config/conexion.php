ab0c179e125822968be1e310d6b70b71537e2981
jhsoft8705 <jhonalexvillaflores@gmail.com>
2023-12-13 12:48:03 -0500

    cambios en config

--- config/conexion.php
+++ config/conexion.php
@@ -7,7 +7,7 @@
             try{
                 /* TODO: Cadena de Conexion */
                 $conectar = $this->dbh=new 
-                PDO("sqlsrv:Server=DESKTOP-18L73MT\SQLEXPRESS;Database=bd_sys_library","sa","123");
+                PDO("sqlsrv:Server=server-01-colegio.database.windows.net;Database=bd_sys_library","administrado-colegio","@Sistema12345");
                  return $conectar;
             }catch (Exception $e){
                 /* TODO: En caso de error mostrar mensaje */
@@ -16,7 +16,16 @@
             }
         } 
         public static function ruta(){ 
-             return "http://localhost/Sistema_gestion_biblioteca_2023/";  
+             return "http://sistema-web-biblioteca.azurewebsites.net/";  
+
+            // return "http://localhost/Sistema_gestion_biblioteca_2023/";  
+
+            //Server:server-01-colegio.database.windows.net
+            //User:administrado-colegio
+            //Pass:@Sistema12345
+
+            //nombre:http://sistema-web-biblioteca.azurewebsites.net
+
         }
     }
 ?>
